47ad1f45d61910fdc85fae74a3867ad469c9e70f
Eric Klicker <ericklicker@yahoo.com>
2016-11-07 19:53:18 -0800

    yes

---
+++ HW 172 06/HW 172 06/Comlex.cpp
@@ -0,0 +1,9 @@
+//
+//  Comlex.cpp
+//  HW 172 06
+//
+//  Created by Eric Klicker on 11/7/16.
+//  Copyright © 2016 Eric Klicker. All rights reserved.
+//
+
+#include "Comlex.hpp"
---
+++ HW 172 06/HW 172 06/Comlex.hpp
@@ -0,0 +1,37 @@
+//
+//  Comlex.hpp
+//  HW 172 06
+//
+//  Created by Eric Klicker on 11/7/16.
+//  Copyright © 2016 Eric Klicker. All rights reserved.
+//
+
+#ifndef Comlex_hpp
+#define Comlex_hpp
+
+#include <stdio.h>
+
+class Complex;
+
+private:
+
+double a;
+double b;
+
+
+public:
+Complex(a,b,);
+Complex(a);
+Complex();
+
+double getRealPart();
+double getImaginaryPart();
+double add;
+double subtract;
+double multiply;
+double abs;
+
+
+
+
+#endif /* Comlex_hpp */
