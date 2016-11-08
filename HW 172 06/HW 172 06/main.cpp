08e7f2bbb6752ff6d540c9232e3033ba95b728b0
Eric Klicker <ericklicker@yahoo.com>
2016-11-07 19:32:21 -0800

    creating repo

---
+++ HW 172 06/HW 172 06/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  HW 172 06
+//
+//  Created by Eric Klicker on 11/7/16.
+//  Copyright © 2016 Eric Klicker. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    
+    
+    cout << "Initial commit" << endl;
+}
